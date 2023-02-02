0cea733e63511320cde468e7f93dadf9ce14ee23
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-02-02 11:03:00 -0600

    1102-loading-added

---
+++ js/script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// const reviewObj = {
+//     restaurant_id: 1,
+//     name: 'Jeremy',
+//     rating: 5,
+//     comments: "first review"
+// };
+// const url = 'https://round-puffy-blizzard.glitch.me/movies';
+// const options = {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(reviewObj),
+// };
+// fetch(url, options)
+//     .then( response => console.log(response) ) /* review was created successfully */
+//     .catch( error => console.error(error) ); /* handle errors */
+
+
+function startDelay(){
+    setTimeout(function() {
+            $('#loading').addClass('hide');
+    }, 2000);
+};
+$(window).load(startDelay());
