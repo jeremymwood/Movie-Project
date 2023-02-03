442d08e5b3c9200d91f04fa65678a6f94a36f0bf
Andrea Varnado <andrea.h.varnado@gmail.com>
2023-02-03 11:40:54 -0600

    edit functionality

--- js/script.js
+++ js/script.js
@@ -1,72 +1,57 @@
 "use strict";
 
-// const reviewObj = {
-//     restaurant_id: 1,
-//     name: 'Jeremy',
-//     rating: 5,
-//     comments: "first review"
-// };
 const url = 'https://round-puffy-blizzard.glitch.me/movies';
-// const options = {
-//     method: 'POST',
-//     headers: {
-//         'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(reviewObj),
-// };
-// fetch(url)
-//     .then( response => console.log(response) ) /* review was created successfully */
-//     .catch( error => console.error(error) ); /* handle errors */
-
+let allMovies;
+// console.log(allMovies);
 
 // loading message function to disappear and show movie lists after 5 seconds
 function startDelay(){
     setTimeout(function() {
         $('#loading').addClass('hide');
         getMovies();
+        // console.log(allMovies);
     }, 2000);
 }
 startDelay();
 
+
 //ajax request to get a listing of all movies
 function getMovies () {
     $.get(url).done(function (data) {
+        allMovies = data;
         //do something with the data
         let movies= "";
-        $.each(data, function(data,value){
+        $.each(data, function(data, value){
             movies += `
            <div class="col">
                 <div class="movieItem card bg-transparent border border-2 d-flex flex-row p-3 w-100 h-100" id="moveieItem${value.id}">
                     <div class="flex-grow-1 pe-3">
                         <div class="cardTitle fs-5 fw-bolder">${value.title}</div>
-                        <div>Rating: ${value.rating}/10</div>
-                        <div>Genre: ${value.genre}</div>
-                        <div>Director: ${value.director}</div>
-                        <div>Staring:${value.staring}</div>
-                        <div class="fst-italic">"${value.tagline}"</div>
+                        <div class="cardRating">Rating: ${value.rating}/10</div>
+                        <div class="cardGenre">Genre: ${value.genre}</div>
+                        <div class="cardDirector">Director: ${value.director}</div>
+                        <div class="cardStaring">Staring:${value.staring}</div>
+                        <div class="cardTagline fst-italic">"${value.tagline}"</div>
                     </div>
                     <div class="controls d-flex flex-column">
-                        <button type="button" class="btn" >
+                        <button type="button" class="btn closeButton" >
                             <i class="fa-solid fa-xmark"></i>
                         </button>
-                        <button type="button" class="btn mb-2" data-bs-toggle="modal" data-bs-target="#editMovieModal">
+                        <button type="button" class="editMovieBtn btn mb-2" data-bs-toggle="modal" data-bs-target="#editMovieModal" data-id="${value.id}">
                             <i class="fa-solid fa-pen"></i>
                         </button>
                     </div>
                 </div>
             </div>
             `;
+
         })
+        console.log(data);
         $("#movieContent").html(movies);
     });
 }
 
-//add movie button functionality
-// $("#addMovieBtn").click(function(){
-//
-// });
-
-//submit button functionality for new movies
+//submit button functionality for adding movies
 $("#addMoviesSubmitBtn").click(function(event){
     event.preventDefault();
     console.log("working");
@@ -82,38 +67,96 @@ $("#addMoviesSubmitBtn").click(function(event){
         body: JSON.stringify(movie),
     };
     fetch(url,options)
-        .then(function(response){
-            // let movieTitle = document.getElementById('mtitle').value;
-            // let movieRating = document.getElementById('mrating').value;
-            // addMovie(movieTitle,movieRating);
-        });
+        .then(function(){
+            getMovies();
+        }).then($("#addMovieModal").modal("toggle"));
+});
+
+function refreshPage(){
+    window.location.reload();
+}
+
+//edit button functionality for editing movies
+let working;
+$("#movieContent").on("click", "button.editMovieBtn", function (event) {
+   event.preventDefault();
+    working = $(this).attr("data-id");
+        $("#editmtitle").val(allMovies[working - 1].title);
+        $("#editmrating").val(allMovies[working - 1].rating);
+        $("#editmgenre").val(allMovies[working - 1].genre);
+        $("#editmdirector").val(allMovies[working - 1].director);
+        $("#editmstaring").val(allMovies[working - 1].staring);
+        $("#editmtagline").val(allMovies[working - 1].tagline);
+        console.log(working);
+});
+
+
+//submit button for editing movies
+$("#editMoviesSubmitBtn").click(function(){
+    $("#editmtitle").val();
+    $("#editmrating").val();
+    $("#editmgenre").val();
+    $("#editmdirector").val();
+    $("#editmstaring").val();
+    $("#editmtagline").val();
+
+    fetch(`https://round-puffy-blizzard.glitch.me/movies/${working}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+            title: document.querySelector('#editmtitle').value,
+            rating: document.querySelector('#editmrating').value,
+            genre: document.querySelector('#editmgenre').value,
+            director: document.querySelector('#editmgenre').value,
+            staring: document.querySelector('#editmgenre').value,
+            tagline: document.querySelector('#editmgenre').value
+        }),
+        headers: {"Content-Type": "application/json"}
+    }).then(function(){
+        getMovies();
+    }).then($("#editMovieModal").modal("toggle"))
+        .catch(error => console.log(error));
 
 });
 
-//submit button functionality for editing movies
-$("#editMoviesSubmitBtn").click(function(event){
-    event.preventDefault();
-    console.log("working");
 
-});
+
 
 //delete button functionality for deleting movie cards
-$("#deleteBtn").click(function(){
-    // event.preventDefault();
-    console.log("working");
-    // $(this).parents("#movieContent").hide();
+$(document).on("click", '.closeButton', function(event){
+    event.preventDefault();
+    let thisCard = this.closest(".movieItem");
+    let thisId = this.closest(".movieItem").id;
+    // let json = {id: thisId};
+    console.log(thisId);
+    thisCard.remove();
+
+    const options = {
+        method: 'DELETE',
+    };
+
+    fetch(url + "/" + thisId, options)
+        .then(/* post was created successfully */)
+        .catch(/* handle errors */);
 });
 
 //search bar functionality
-$("#searchBtn").click(function(event){
+// let movieSelection = document.querySelector("#movieSelection");
+// movieSelection.addEventListener("change",updateMovies);
+// function updateMovies() {
+//     let selectedMovie = movieSelection.value;
+//     let filteredMovies = [];
+//     let recorded = document.getElementById("searchbar").value.trim();
+//
+// }
+$("#searchBtn").click(function (event) {
     console.log("working");
     let input = document.getElementById("searchInput").value;
     input.toLowerCase();
     let movieTitles = document.getElementsByClassName("movieTitles");
     for (let i = 0; i < movieTitles.length; i++) {
-        if(!movieTitles[i].innerHTML.toLowerCase().includes(input)){
+        if (!movieTitles[i].innerHTML.toLowerCase().includes(input)) {
             movieTitles[i].style.display = "none";
-        }else {
+        } else {
             movieTitles[i].style.display = "block";
         }
     }
@@ -134,24 +177,6 @@ $("#genreFilter").click(function(){
     console.log("working");
 });
 
-// function to add a movie; possibly useful later
-// function addMovie(inputTitle, inputRating){
-//     let movie = {};
-//     let lastID = movies.length;
-//     movie.id = lastID+1;
-//     movie.title = inputTitle;
-//     movie.rating = inputRating;
-//     console.log(movie);
-//     $("#movieContent").prepend($("movie"));
-// }
 
-//edit modal functionality
-let editModal = document.querySelector("#editMovieModal");
-let editModalBtn = document.querySelector("#editMoviesSubmitBtn");
 
-// $('.card').hover(.css('color', random_color));
-var hexArray = ['#hexVal','#hexVal','#hexval', '#hexval']
-var randomColor = hexArray[Math.floor(Math.random() * hexArray.length)];
 
-$("#divId").css("color",randomColor); //A class selector would work too
-$("#divId").css("color",randomColor); //A class selector would work too
